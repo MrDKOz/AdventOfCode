35469b704e6adcc9da516069ac08eede623922ef
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-01 09:09:18 +0000

    [Completed] Year: 2023, Day: One

---
+++ 2023/DayOne.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode.Helpers;
+
+namespace AdventOfCode._2023;
+
+public class DayOne
+{
+    private CalibrationValues? _calibrationValues;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calibrationValues = new CalibrationValues(PuzzleInput.Load(2023, 1));
+    }
+    
+    [Test]
+    public void PartOne()
+    {
+        Console.WriteLine($"Day One, Part One Answer: {_calibrationValues!.ProcessedValues.Sum()}");
+    }
+
+    [Test]
+    public void PartTwo()
+    {
+        
+    }
+}
+
+internal class CalibrationValues
+{
+    private readonly List<string> _rawInput;
+    public readonly List<int> ProcessedValues = new();
+
+    public CalibrationValues(List<string> rawInput)
+    {
+        _rawInput = rawInput;
+
+        ExtractFirstAndLastDigits();
+    }
+
+    private void ExtractFirstAndLastDigits()
+    {
+        
+        foreach (var line in _rawInput)
+        {
+            var firstDigit = FindFirstDigit(line);
+            var lastDigit = FindLastDigit(line);
+            
+            ProcessedValues.Add(Convert.ToInt32($"{firstDigit}{lastDigit}"));
+        }
+    }
+
+    private static string FindFirstDigit(string input)
+    {
+        foreach (var character in input.Where(char.IsDigit))
+        {
+            return character.ToString();
+        }
+
+        throw new Exception("No digit found.");
+    }
+
+    private static string FindLastDigit(string input) => FindFirstDigit(new string(input.Reverse().ToArray()));
+}
