1a8c96c0637721e06df26caea3ee3204971ff9e1
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-01 08:22:17 +0000

    [Added] Year: 2022, Day: Eight

---
+++ 2022/Day8.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode._2022;
+
+public class Forest
+{
+    
+}
+
+public class Day8
+{
+    private static readonly List<string> PuzzleInput = Helpers.PuzzleInput.Load(2022, 8);
+
+    [Test]
+    public void PartOne()
+    {
+        
+    }
+}
