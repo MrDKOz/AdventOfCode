91183d4e3bd967417ffb8308a823314f6ec935f5
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-06 16:30:32 +0000

    [Added] Year: 2023, Day: Six

---
+++ 2023/DaySix.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode._2023;
+
+public class DaySix
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void PartOne()
+    {
+
+    }
+
+    [Test]
+    public void PartTwo()
+    {
+
+    }
+}
