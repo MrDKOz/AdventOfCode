ba17bc8d858def3aff9e0ba35bab2399092728fd
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-07 09:12:55 +0000

    [Completed] Year: 2023, Day: Six, Part: One

--- 2023/DaySix.cs
+++ 2023/DaySix.cs
@@ -1,22 +1,87 @@
-﻿namespace AdventOfCode._2023;
+﻿using AdventOfCode.Helpers;
+
+namespace AdventOfCode._2023;
 
 public class DaySix
 {
+    private Races? _races;
+    
     [SetUp]
     public void Setup()
     {
-
+        _races = new Races(PuzzleInput.Load(2023, 6));
     }
 
     [Test]
     public void PartOne()
     {
-
+        Console.WriteLine($"Day Six, Part One Answer: {_races?.TotalWinCount}");
     }
 
     [Test]
     public void PartTwo()
     {
 
+    }
+}
+
+public class Races
+{
+    private readonly List<Race> RaceList = new();
+    public int TotalWinCount => RaceList
+        .Select(r => r.WinningButtonHoldTimesCount)
+        .Aggregate(1, (total, next) => total * next);
+
+    public Races(IReadOnlyCollection<string> input)
+    {
+        ProcessInput();
+        return;
+
+        void ProcessInput()
+        {
+            var times = input.First().Split(':').Last().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var distances = input.Last().Split(':').Last().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < times.Length; i++)
+            {
+                AddRace(times[i], distances[i]);
+            }
+        }
+    }
+
+    private void AddRace(string time, string distance) => 
+        RaceList.Add(new Race(float.Parse(time), float.Parse(distance)));
+
+    private class Race
+    {
+        private readonly float _time;
+        private readonly float _distance;
+        private List<int> WinningButtonHeldTimes { get; set; } = new();
+        public int WinningButtonHoldTimesCount => WinningButtonHeldTimes.Count;
+
+        public Race(float time, float distance)
+        {
+            _time = time;
+            _distance = distance;
+
+            CalculatePossibleButtonHoldTimes();
+        }
+
+        private void CalculatePossibleButtonHoldTimes()
+        {
+            var buttonHoldTime = 0;
+
+            for (var i = 0; i < _time; i++)
+            {
+                var distance = buttonHoldTime * (_time - i);
+
+                if (distance > _distance)
+                {
+                    WinningButtonHeldTimes.Add(buttonHoldTime);
+                }
+
+                buttonHoldTime++;
+            }
+        }
     }
 }
