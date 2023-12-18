ff5de268e39e4f7d6bb7ad127e7ec1f02bae86a6
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-18 14:57:35 +0000

    [Completed] Year: 2023, Day: Eight, Part: One

--- 2023/DayEight.cs
+++ 2023/DayEight.cs
@@ -15,7 +15,7 @@ public class DayEight
     [Test]
     public void PartOne()
     {
-        Console.WriteLine($"Day Eight, Part One Answer: {_map?.PartOne()}");
+        Console.WriteLine($"Day Eight, Part One Answer: {_map?.PartOne("AAA", "ZZZ")}");
     }
 
     [Test]
@@ -27,102 +27,37 @@ public class DayEight
 
 public class Map
 {
-    private NodePart[] _instructions = null!;
-    private Dictionary<string, NodeValues> _nodes;
-    private string _startingLabel = string.Empty;
+    private readonly int[] _instructions;
+    private readonly Dictionary<string, (string left, string right)> _nodes;
 
-    public Map(List<string> input)
+    public Map(IReadOnlyList<string> input)
     {
-        ProcessInput(input);
+        _instructions = input[0].Trim().Select(i => i == 'L' ? 0 : 1).ToArray();
+        _nodes = new Dictionary<string, (string left, string right)>(input.Count);
+
+        for (var i = 2; i < input.Count; i++)
+        {
+            var line = input[i];
+            _nodes.Add(line.Substring(0, 3), (line.Substring(7, 3), line.Substring(12, 3)));
+        }
     }
 
-    private void ProcessInput(List<string> input)
-    {
-        _nodes = new Dictionary<string, NodeValues>(input.Count);
-
-        foreach (var line in input)
-        {
-            if (line == input.First())
-            {
-                ProcessInstructions(line);
-            }
-            else
-            {
-                ProcessNodes(line);
-            }
-        }
-
-        return;
-
-        void ProcessInstructions(string line)
-        {
-            var length = line.Length;
-            _instructions = new NodePart[length];
-
-            for (var i = 0; i < length; i++)
-            {
-                _instructions[i] = line[i] == 'L'
-                    ? NodePart.Left
-                    : NodePart.Right;
-            }
-        }
-
-        void ProcessNodes(string line)
-        {
-            if (string.IsNullOrEmpty(line)) return;
-
-            if (_startingLabel == string.Empty) _startingLabel = GetNodePart(NodePart.Label, line);
-
-            _nodes.Add(GetNodePart(NodePart.Label, line), new NodeValues(GetNodePart(NodePart.Left, line), GetNodePart(NodePart.Right, line)));
-        }
-
-        string GetNodePart(NodePart part, string fetchFrom) =>
-            part switch
-            {
-                NodePart.Label => fetchFrom[..3],
-                NodePart.Left => fetchFrom[7..10],
-                NodePart.Right => fetchFrom[12..15],
-                _ => throw new Exception($"No part found for {part}")
-            };
-    }
-
-    public long PartOne()
+    public long PartOne(string start, string end)
     {
         long stepCount = 0;
-        var currentValue = _startingLabel;
-        var target = new HashSet<string> { "ZZZ" };
+        var currentValue = start;
 
-        while (true)
+        do
         {
-            foreach (var instruction in _instructions)
-            {
-                if (target.Contains(currentValue)) return stepCount;
+            var tempInstructionIndex = stepCount % _instructions.Length;
 
-                currentValue = GetNodeSide(currentValue, instruction);
+            currentValue = _instructions[tempInstructionIndex] == 0
+                ? _nodes[currentValue].left
+                : _nodes[currentValue].right;
 
-                stepCount++;
-            }
-        }
-    }
+            stepCount++;
+        } while (currentValue != end);
 
-    private string GetNodeSide(string label, NodePart part)
-    {
-        if (_nodes.TryGetValue(label, out var nodeValues))
-        {
-            return part == NodePart.Left
-                ? nodeValues.LeftSide
-                : nodeValues.RightSide;
-        }
-
-        throw new Exception($"Node not found with label {label}");
-    }
-
-    private record NodeValues(string LeftSide, string RightSide);
-    
-    private enum NodePart
-    {
-        Label,
-        Left,
-        Right
+        return stepCount;
     }
 }
