34476f3d0fd915e0c0aa5370e6a6276a84946b5b
Dan Osborne <daniel.osborne@red-gate.com>
2023-12-19 09:37:52 +0000

    Working on day 9

--- 2023/DayNine.cs
+++ 2023/DayNine.cs
@@ -16,7 +16,7 @@ public class DayNine
     [Test]
     public void PartOne()
     {
-        Console.WriteLine($"Day Eight, Part One Answer: ");
+        Console.WriteLine($"Day Eight, Part One Answer: {oasis.PartOne()}");
     }
 
     [Test]
@@ -27,22 +27,66 @@ public class DayNine
 
     private class Oasis
     {
-        private readonly int[,] _readings;
+        private readonly List<int[]> _readings;
 
-        public Oasis(IReadOnlyList<string> input)
+        public Oasis(IReadOnlyCollection<string> input)
         {
-            var digitCount = input[0].Split(' ').Length;
-            _readings = new int[input.Count, digitCount];
+            _readings = new List<int[]>(input.Count);
 
-            for (var i = 0; i < input.Count; i++)
+            foreach (var line in input)
             {
-                for (var j = 0; j < digitCount; j++)
-                {
-                    _readings[i, j] = int.Parse(input[i].Split(' ')[j]);
-                }
+                _readings.Add(line.Split(' ').Select(int.Parse).ToArray());
             }
-            
-            Console.WriteLine("hello");
+        }
+
+        public int PartOne()
+        {
+            foreach (var reading in _readings)
+            {
+                var sequences = GenerateSequences(reading);
+                var nextValue = CalculateNextValue(sequences);
+            }
+
+            return 0;
+        }
+
+        private static IReadOnlyList<int[]> GenerateSequences(IReadOnlyList<int> reading)
+        {
+            var sequences = new List<int[]>();
+            var firstSequence = true;
+            do
+            {
+                sequences.Add(firstSequence
+                    ? CalculateDifferences(reading)
+                    : CalculateDifferences(sequences.Last()));
+
+                firstSequence = false;
+            } while (sequences.Last().Sum() > 0);
+
+            return sequences;
+        }
+
+        private static int CalculateNextValue(IReadOnlyList<int[]> sequences)
+        {
+            for (var i = sequences.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(sequences[i].Sum());
+            }
+
+            return 0;
+        }
+
+
+        private static int[] CalculateDifferences(IReadOnlyList<int> input)
+        {
+            var returnValue = new int[input.Count - 1];
+
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                returnValue[i] = input[i + 1] - input[i];
+            }
+
+            return returnValue;
         }
     }
 }
