1558f0cd8837a03dff358a38d9db714f274e0b1b
Dan Osborne <daniel.osborne@red-gate.com>
2024-12-02 09:54:11 +0000

    [Completed] Year: 2024, Day: Two, Part: Two

--- 2024/DayTwo.cs
+++ 2024/DayTwo.cs
@@ -12,27 +12,17 @@ public class DayTwo : ExerciseBase
     [Test, Description("Answer: 490")]
     public override void PartOne() => Console.WriteLine($"Day Two, Part One Answer: {_report.SafeEntries()}");
 
-    [Test]
-    public override void PartTwo()
-    {
-        throw new NotImplementedException();
-    }
+    [Test, Description("Answer: 536")]
+    public override void PartTwo() => Console.WriteLine($"Day Two, Part Two Answer: {_report.SafeDampenedEntries()}");
 
     private class Report
     {
         private readonly List<ReportEntry> _entries;
         public int SafeEntries() => _entries.Count(entry => entry.IsSafe());
+        public int SafeDampenedEntries() => _entries.Count(entry => entry.IsSafeWithDampening());
 
         public Report(IReadOnlyList<string> rawList) =>
             _entries = rawList.Select(entry => new ReportEntry(entry)).ToList();
-
-        public void ProblemDampener()
-        {
-            foreach (var entry in _entries.Where(entry => !entry.IsSafe()))
-            {
-
-            }
-        }
     }
 
     private class ReportEntry(string entry)
@@ -40,42 +30,42 @@ public class DayTwo : ExerciseBase
         private readonly List<int> _readings =
             entry.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
-        public bool IsSafe()
+        public bool IsSafe() => IsRecordSafe(_readings);
+
+        private static bool IsRecordSafe(List<int> toTest)
         {
-            var allAscending = AllAscending();
-            var allDescending = AllDescending();
-            var safeLevelDifferences = SafeLevelDifferences();
+            var allAscending = toTest.SequenceEqual(toTest.OrderBy(x => x));
+            var allDescending = toTest.SequenceEqual(toTest.OrderByDescending(x => x));
+            var safeDifferences = true;
 
-            // Console.WriteLine(string.Join(", ", _readings));
-            // Console.WriteLine($"Ascending: {allAscending}, Descending: {allDescending}, Safe Differences: {safeLevelDifferences}");
+            for (var i = 0; i < toTest.Count - 1; i++)
+            {
+                var difference = Math.Abs(toTest[i] - toTest[i + 1]);
 
-            return (allAscending || allDescending) && safeLevelDifferences;
+                if (difference is < 1 or > 3)
+                {
+                    safeDifferences = false;
+                }
+            }
+
+            return (allAscending || allDescending) && safeDifferences;
         }
 
         public bool IsSafeWithDampening()
         {
             if (IsSafe()) return true;
 
-
-        }
-
-        private bool AllAscending() => _readings.SequenceEqual(_readings.OrderBy(x => x));
-
-        private bool AllDescending() => _readings.SequenceEqual(_readings.OrderByDescending(x => x));
-
-        private bool SafeLevelDifferences()
-        {
-            for (var i = 0; i < _readings.Count - 1; i++)
+            for (var i = 0; i < _readings.Count; i++)
             {
-                var difference = Math.Abs(_readings[i] - _readings[i + 1]);
+                var testRecord = new List<int>();
+                testRecord.AddRange(_readings);
+                testRecord.RemoveAt(i);
 
-                if (difference is < 1 or > 3)
-                {
-                    return false;
-                }
+                if (IsRecordSafe(testRecord))
+                    return true;
             }
 
-            return true;
+            return false;
         }
     }
 }
