36b8ce7d889c568ea2509bd13f9fc62b6f166028
Dan Osborne <daniel.osborne@pm.me>
2024-12-03 08:53:19 +0000

    [Completed] Year: 2024, Day: Three, Part: One

---
+++ 2024/DayThree.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2024;
+
+public class DayThree : ExerciseBase
+{
+    private readonly string _singleLine;
+
+    public DayThree() : base(2024, 3)
+    {
+        _singleLine = string.Join("", Input);
+    }
+
+    [Test, Description("Answer: 174336360")]
+    public override void PartOne()
+    {
+        var result = 0;
+
+        var matches = GetMatches(_singleLine);
+
+        foreach (Match match in matches)
+        {
+            result += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+        }
+
+        Console.WriteLine($"Day Three, Part One Answer: {result}");
+    }
+
+    public override void PartTwo()
+    {
+    }
+
+    private MatchCollection GetMatches(string input)
+    {
+        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+
+        return regex.Matches(input);
+    }
+}
