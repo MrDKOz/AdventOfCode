485714f9c85df0265cc4a452191cad666a9defde
Dan Osborne <daniel.osborne@red-gate.com>
2024-12-05 11:14:03 +0000

    [Completed] Year: 2024, Day: Five, Part: Two

--- 2024/DayFive.cs
+++ 2024/DayFive.cs
@@ -2,29 +2,24 @@
 
 public class DayFive : ExerciseBase
 {
-    private InstructionBook _instructionBook;
+    private readonly (int right, int wrong) _answers;
 
     public DayFive() : base(2024, 5)
     {
-        _instructionBook = new InstructionBook(Input);
+        var instructionBook = new InstructionBook(Input);
+        _answers = instructionBook.Process();
     }
 
-    [Test]
-    public override void PartOne()
-    {
-        Console.WriteLine($"Day Four, Part One Answer: {_instructionBook.StepOne()}");
-    }
+    [Test, Description("Answer: 5948")]
+    public override void PartOne() => Console.WriteLine($"Day Four, Part One Answer: {_answers.right}");
 
-    [Test]
-    public override void PartTwo()
-    {
-        throw new NotImplementedException();
-    }
+    [Test, Description("Answer: 3062")]
+    public override void PartTwo() => Console.WriteLine($"Day Four, Part Two Answer: {_answers.wrong}");
 
     private class InstructionBook
     {
         private readonly PageOrderRules _pageOrderRules = new();
-        private List<PageUpdates> _updates = [];
+        private readonly List<PageUpdates> _updates = [];
 
         public InstructionBook(IReadOnlyList<string> input)
         {
@@ -46,40 +41,44 @@ public class DayFive : ExerciseBase
             }
         }
 
-        public int StepOne()
+        public (int right, int wrong) Process()
         {
-            var result = 0;
+            var right = 0;
+            var wrong = 0;
 
             foreach (var update in _updates)
             {
                 var updated = false;
-                var allRules = _pageOrderRules.FindRules(update._pagesToUpdate);
+                var allRules = _pageOrderRules.FindRules(update.GetPages());
 
-                for (var i = 0; i < update._pagesToUpdate.Count; i++)
+                for (var i = 0; i < update.GetCount(); i++)
                 {
-                    var value = update._pagesToUpdate[i];
+                    var value = update.GetValueAtIndex(i);
                     var ruleExists = allRules.TryGetValue(value, out var relatedRule);
 
                     if (!ruleExists) continue;
 
-                    foreach (var rule in relatedRule!)
+                    foreach (var rule in relatedRule!.Where(rule => update.IsAfter(value, rule)))
                     {
-                        if (update.IsAfter(value, rule))
-                        {
-                            update.MoveToBefore(value, rule);
-                            updated = true;
-                            i = 0;
-                        }
+                        update.MoveToBefore(value, rule);
+                        updated = true;
+                        i = 0;
                     }
                 }
 
-                if (updated) continue;
-
-                var middlePage = update._pagesToUpdate[update._pagesToUpdate.Count / 2];
-                result += middlePage;
+                var middlePage = update.GetMiddleValue();
+                
+                if (updated)
+                {
+                    wrong += middlePage;
+                }
+                else
+                {
+                    right += middlePage;
+                }
             }
 
-            return result;
+            return (right, wrong);
         }
 
         private class PageOrderRules
@@ -123,21 +122,27 @@ public class DayFive : ExerciseBase
 
                 values.Add(value);
             }
-
-            public int GetCount() => _pageOrderRules.Count;
         }
 
         private class PageUpdates
         {
-            public List<int> _pagesToUpdate = [];
+            private readonly List<int> PagesToUpdate = [];
 
             public PageUpdates(string pagesToUpdate)
             {
                 foreach (var pageNumber in pagesToUpdate.Split(','))
                 {
-                    _pagesToUpdate.Add(int.Parse(pageNumber));
+                    PagesToUpdate.Add(int.Parse(pageNumber));
                 }
             }
+
+            public List<int> GetPages() => PagesToUpdate;
+            
+            public int GetValueAtIndex(int index) => PagesToUpdate[index];
+            
+            public int GetMiddleValue() => GetValueAtIndex(GetCount() / 2);
+            
+            public int GetCount() => PagesToUpdate.Count;
 
             public bool IsBefore(int toCheck, int target) => IndexOf(toCheck) < IndexOf(target);
 
@@ -151,8 +156,8 @@ public class DayFive : ExerciseBase
                 if (toMoveIndex == -1 || beforeIndex == -1)
                     throw new Exception("One or both of the values do not exist in the list.");
 
-                _pagesToUpdate.RemoveAt(toMoveIndex);
-                _pagesToUpdate.Insert(IndexOf(before), toMove);
+                PagesToUpdate.RemoveAt(toMoveIndex);
+                PagesToUpdate.Insert(IndexOf(before), toMove);
             }
 
             public void MoveToAfter(int toMove, int after)
@@ -163,17 +168,17 @@ public class DayFive : ExerciseBase
                 if (toMoveIndex == -1 || afterIndex == -1)
                     throw new Exception("One or both of the values do not exist in the list.");
 
-                _pagesToUpdate.RemoveAt(toMoveIndex);
+                PagesToUpdate.RemoveAt(toMoveIndex);
 
-                if (afterIndex == _pagesToUpdate.Count)
-                    _pagesToUpdate.Add(toMove);
+                if (afterIndex == PagesToUpdate.Count)
+                    PagesToUpdate.Add(toMove);
                 else
-                    _pagesToUpdate.Insert(afterIndex + 1, toMove);
+                    PagesToUpdate.Insert(afterIndex + 1, toMove);
             }
 
             private int IndexOf(int pageNumber)
             {
-                return _pagesToUpdate.IndexOf(pageNumber);
+                return PagesToUpdate.IndexOf(pageNumber);
             }
         }
     }
