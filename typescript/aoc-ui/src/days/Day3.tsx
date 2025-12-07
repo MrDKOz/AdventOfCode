659b040e8be2f8b75efa3b4fb6c06d6112193c80
Daniel Osborne <daniel.osborne@pm.me>
2025-12-07 12:09:29 +0000

    Refactors day 3 solution to use new part 2 function

--- typescript/aoc-ui/src/days/Day3.tsx
+++ typescript/aoc-ui/src/days/Day3.tsx
@@ -2,41 +2,6 @@ import { DayTemplate } from "../components/DayTemplate";
 import type { SolverFunction } from "../types/SolverFunction";
 import { getLines } from "../utils/input";
 
-const solverPart1: SolverFunction = (input) => {
-  const lines = getLines(input);
-
-  const maxPairsForLine = (line: string): number => {
-    if (line.split("9").length > 2) {
-      return 99;
-    }
-
-    let max = 0;
-
-    for (let i = 0; i < line.length - 1; i++) {
-      const number = line[i];
-
-      for (let j = i + 1; j < line.length; j++) {
-        const candidate = Number(number + line[j]);
-
-        if (candidate > max) {
-          max = candidate;
-        }
-      }
-    }
-
-    return max;
-  }
-
-  const processLines = lines.reduce((totalJoltage, line) => {
-    return totalJoltage + maxPairsForLine(line);
-  }, 0);
-
-  return `Total Joltage: ${processLines}`;
-};
-
-const solverPart2: SolverFunction = (input) => {
-  const lines = getLines(input);
-
   const maxValueForLine = (line: string, requiredLength: number): number => {
     const currentValue: string[] = [];
     let dropsLeft = line.length - requiredLength;
@@ -57,6 +22,17 @@ const solverPart2: SolverFunction = (input) => {
     return Number(currentValue.slice(0, requiredLength).join(''));
   }
 
+const solverPart1: SolverFunction = (input) => {
+  const lines = getLines(input);
+  const processLines = lines.reduce((totalJoltage, line) => {
+    return totalJoltage + maxValueForLine(line, 2);
+  }, 0);
+
+  return `Total Joltage: ${processLines}`;
+};
+
+const solverPart2: SolverFunction = (input) => {
+  const lines = getLines(input);
   const processLines = lines.reduce((totalJoltage, line) => {
     return totalJoltage + maxValueForLine(line, 12);
   }, 0);
